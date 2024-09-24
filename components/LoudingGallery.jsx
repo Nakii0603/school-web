69bb40c22481ec5b5309177123032af60128f2fa
Nakii <guest1@Naakiizs-MacBook-Air.local>
2024-09-24 20:56:19 +0800

    add file

--- components/LoudingGallery.jsx
+++ components/LoudingGallery.jsx
@@ -1,9 +1,8 @@
 import React, { useState, useEffect } from "react";
 import ImgGallery from "./ImgGallery";
-import App from "./Loud";
 import Loading from "./Loud";
 
-export default function LoudingGallery() {
+export default function LoadingGallery() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -14,22 +13,20 @@ export default function LoudingGallery() {
     return () => clearTimeout(timer);
   }, []);
 
+  const loadingItems = Array.from({ length: 4 }); 
+
   return (
     <div className="flex justify-center">
       {loading ? (
         <div className="flex flex-col gap-[5px]">
-          <div className="flex gap-[5px]">
-            <Loading />
-            <Loading />
-            <Loading />
-            <Loading />
-          </div>
-          <div className="flex gap-[5px]">
-            <Loading />
-            <Loading />
-            <Loading />
-            <Loading />
-          </div>
+          {loadingItems.map((_, index) => (
+            <div className="flex gap-[5px]" key={index}>
+              <Loading />
+              <Loading />
+              <Loading />
+              <Loading />
+            </div>
+          ))}
         </div>
       ) : (
         <div>
