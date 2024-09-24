89046bc27614d83853a08791418f204a5844d9d9
Nakii <guest1@Naakiizs-MacBook-Air.local>
2024-09-24 20:40:20 +0800

    add files

--- components/LoudingGallery.jsx
+++ components/LoudingGallery.jsx
@@ -9,7 +9,7 @@ export default function LoudingGallery() {
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 3000);
+    }, 5000);
 
     return () => clearTimeout(timer);
   }, []);
